e73c39da6bfd4f252a792a02e1eeff8def4bbee6
Ilya-Yanin <yaninilya0501@gmail.com>
2025-11-03 23:32:47 +0200

    Домашка № 1 по 6 заданию + теория по 6 заданию

---
+++ Task6/17860.py
@@ -0,0 +1,32 @@
+from turtle import *
+
+screensize(3000, 3000)
+tracer(False)
+
+m = 5
+
+for i in range(9):
+    fd(22 * m)
+    rt(90)
+    fd(6 * m)
+    rt(90)
+up()
+fd(1 * m)
+rt(90)
+fd(5 * m)
+lt(90)
+down()
+for i in range(9):
+    fd(53 * m)
+    rt(90)
+    fd(75 * m)
+    rt(90)
+
+up()
+
+for x in range(-35, 25):
+    for y in range(-10, 80):
+        goto(x*m, y*m)
+        dot(3, 'red')
+update()
+done()
